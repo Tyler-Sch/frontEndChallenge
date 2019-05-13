8d903ecfb6cd0c9b3f5da8e9b2904fc2dbfb3510
Tyler <tylerschauer@gmail.com>
2019-05-13 10:53:51 -0400

    added condition to useEffect so it doesnt fire constantly

--- src/components/form/formComponents/requiredHook.js
+++ src/components/form/formComponents/requiredHook.js
@@ -4,12 +4,13 @@ export default function useHasText(val) {
   const [hasText, setHasText] = useState(false);
 
   useEffect(() => {
+    console.log('use effect fired');
     if (val.length > 0) {
       setHasText(true);
     }
     else if (val.length === 0) {
       setHasText(false);
     }
-  })
+  }, [val.length])
   return hasText;
 }
